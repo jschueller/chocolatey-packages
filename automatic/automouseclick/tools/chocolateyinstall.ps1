f381d9393e3cc326753cbff3cca54ccd21f99de6
Chocolatey <chocolatey@realdimensions.net>
2021-07-30 16:24:30 +0000

    AU: 2 updated - automouseclick cudatext [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f038a9bade000e00700871a69cf5b94ec2f2f044

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '55B31772AFD579F1500D573CBCA9D064E1FFA07BDAC7C1DC124B0638434E2BD9'
+  checksum     = '0828BB6D2FE6F6CF5E4FAF4F1DC7CE9B79FB1BF13F5F8593E688EE12A61DAA4C'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
