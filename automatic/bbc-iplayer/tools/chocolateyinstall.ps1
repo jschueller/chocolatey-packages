6d8c21bb78f7b9b8927e91325a93aeac7fe8f1cc
Chocolatey <chocolatey@realdimensions.net>
2021-09-01 08:24:44 +0000

    AU: 1 updated - bbc-iplayer [skip ci]

--- automatic/bbc-iplayer/tools/chocolateyinstall.ps1
+++ automatic/bbc-iplayer/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  url           = 'https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/win32-x64/BBCiPlayerDownloadsSetup-2.13.3.exe'
-  checksum      = 'b91bfb30322582b921ec7ec50326482112a29561e1496851db1a6d661cfd6532'
+  url           = 'https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/win32-x64/BBCiPlayerDownloadsSetup-2.13.4.exe'
+  checksum      = '7733afc644cd559ab18cbc3c6c03b71e8a4110b49ce9b5b1e0b99b4fd61de860'
   checksumType  = 'sha256'
 }
 
@@ -16,7 +16,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  file          = "$toolsDir\bbciplayerdownloads-2.13.3-full.nupkg" 
+  file          = "$toolsDir\bbciplayerdownloads-2.13.4-full.nupkg" 
 }
 
 Install-ChocolateyZipPackage @packageArgs
