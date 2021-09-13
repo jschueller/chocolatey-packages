2129cc336a006e19337632f7cc03e96c80b16991
Chocolatey <chocolatey@realdimensions.net>
2021-09-13 16:24:59 +0000

    AU: 6 updated - geogebra6 geogebra-geometry phonerlite phonerlite.install phonerlite.portable wireguard [skip ci]

--- automatic/phonerlite.install/tools/chocolateyinstall.ps1
+++ automatic/phonerlite.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName    
-  file          = "$toolsDir\PhonerLiteSetup_2.93.exe"    
+  file          = "$toolsDir\PhonerLiteSetup_2.94.exe"    
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
