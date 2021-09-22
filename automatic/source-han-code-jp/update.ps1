d02b26f3aa3ad259431d06024e2668d70c50d127
Jakub Levý <jakub.levy15@gmail.com>
2021-09-22 13:37:28 +0200

    fix update.ps1

--- automatic/source-han-code-jp/update.ps1
+++ automatic/source-han-code-jp/update.ps1
@@ -2,10 +2,10 @@
 
 function global:au_GetLatest {
   $github_repository = "adobe-fonts/source-han-code-jp"
-  $releases = "https://github.com/" + $github_repository + "/releases/latest"
-  $regex   = "/(?<Version>[\d\.]+)[A-Z]*.zip$"
+  $releases = "https://github.com/" + $github_repository + "/releases"
+  $regex   = "/(?<Version>[\d\.]+)R.zip$"
 
-  $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+  $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select-Object -First 1
   return @{
     Version = $matches.Version
     URL32 = "https://github.com" + $url.href
@@ -15,7 +15,7 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }        
     }
