3405405fd1eb05287c760e2f9bc48bf5b165bd0b
Chocolatey <chocolatey@realdimensions.net>
2021-09-23 00:29:36 +0000

    AU: 3 updated - chirp keeper webex [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/112a19f61c3c9675bee936769d4b6dbc3af13d51

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '3a0568c3b5203d5c23e4afe4ca53a464a60d7a457a65259ac49bb53cd0ffd59b'
+  checksum      = '1bc4ded18639c42ba1068ca77714bee206aa99e4294db3f70503a3de986b8d85'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'b1eebbf240f904db9eba68927a6086d6c32ed2636979341c8eedf474c466ba32'
+  checksum64    = '10790fdf0e021bfa4e282b662040178c38462d509a46779ba58abe9ccbe6714d'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = '68F7F1F211022AE4BBAE77B7493A6C8194EF8FB162FC2FCF0B0A7964BF95C20B'
+  checksum      = 'D2E722E3D5938463C66A8BF001C602EEDDA13FB3F90605B7D86889B10447E467'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = '95D4A2EF923D5EAD4291B110CB8676BE613D4FDBA73A949E9E4457C3AD7AC41E'
+  checksum64    = '523D6EAE98C1949CFC0837EDFCC508DACF0AAD52A0B3F204EB60C4EB564F131B'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
