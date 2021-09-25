a74939fa2297ff42ac53a1bd78ae1688dd76ff65
Chocolatey <chocolatey@realdimensions.net>
2021-09-25 08:25:37 +0000

    AU: 2 updated - keeper wireguard [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e1311d58cab7f0709b50a2d37d8066b0094f8a44

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '1bc4ded18639c42ba1068ca77714bee206aa99e4294db3f70503a3de986b8d85'
+  checksum      = '3fca9ad682b5b0cc3991d31ce3150f728fc9bfc566e9fca58762c6a10c5b1127'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '10790fdf0e021bfa4e282b662040178c38462d509a46779ba58abe9ccbe6714d'
+  checksum64    = 'e5e0d4e7faf095ffa6998b92fcb359bd5020285e88012c8e452b6212d35f8355'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
