63562137701b72b61e61d17274711f31c11e70c3
Chocolatey <chocolatey@realdimensions.net>
2021-09-29 08:25:03 +0000

    AU: 1 updated - helix-alm-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fbe30beb36831d8426c6bf9388cab80a4d7b3f2b

--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2021.2.0/ttwinclientinstall.exe'
-  checksum     = '43b689192d41b1aa4b96b60f523ae78b5813ec03cd11712f3bfc66635e2340fb'
+  url          = 'https://cdist2.perforce.com/alm/helixalm/r2021.2.1/ttwinclientinstall.exe'
+  checksum     = 'f2054e97c512f09a554a965b9a7470a7789ae8b96e939be29d670ec0dbb273c2'
   checksumType = 'sha256'
 
   silentArgs   = "-i SILENT LAX_VM resource\jre\bin\java.exe"
