cb8ff52207e062d96e499a69c81486e4f3ee7f4e
Chocolatey <chocolatey@realdimensions.net>
2021-09-30 16:26:00 +0000

    AU: 8 updated - chirp chirp.install chirp.portable ospray ospray.install ospray.portable spacedesk-client spacedesk-server [skip ci]

--- automatic/spacedesk-client/tools/chocolateyinstall.ps1
+++ automatic/spacedesk-client/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'
 
-  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0931_BETA.msi'
-  checksum      = 'f17153209c300c954873b619d0e45344e00dc75218411e60e4f00f68e76e3127'
+  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0932_BETA.msi'
+  checksum      = 'b01e055fbdef394cdf69e32c7eb0d817867c0e31d13d9eca3f63d48455e9750a'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
