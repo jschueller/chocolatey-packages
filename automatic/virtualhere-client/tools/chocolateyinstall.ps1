46dd2a2aaf599e6c6065e316e9dee62b2861470b
Chocolatey <chocolatey@realdimensions.net>
2021-10-03 16:26:02 +0000

    AU: 3 updated - cudatext klog virtualhere-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/a3ef936901a08597018b1d5d0a13bbe20f571896

--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
   url            = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui32.exe'
-  checksum       = '26b44090a38b65a05ba578cce8b97e0e7dccf8f5d9d74e4ac3055b2d5564a763'
+  checksum       = 'c7307ccf3c95c3e905cdb555d661fff35086f8847456b3ed0c157aec6815c700'
   checksumType   = 'sha256'
 
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
-  checksum64     = 'd4ebc70c1a36231e957a1eefe600538706800ed7a1dc372526e7ffd04054e1d4'
+  checksum64     = '81c856a6fe8550c6af709c7e37189a62661caf3f27b1fb17d449165937fc5a39'
   checksumType64 = 'sha256'
 }
 
