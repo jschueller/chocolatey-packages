ff750a448171a8205024a21e9f29ebddc0093a7c
Chocolatey <chocolatey@realdimensions.net>
2021-10-09 16:25:00 +0000

    AU: 2 updated - myfamilytree myfamilytree-languagepack [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/9097a60a859b0290241c136a4c8be6ca0d806f7d

--- automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType		= "msi"
   
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack11.0.3000.0.msi'
-  checksum      = '420971180aa46c51fbb126f6c800e80861af4334ba45074f117196092b992575'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack11.1.0000.0.msi'
+  checksum      = '00952c9a75cba28b4fc6d1c2481d59b24c5705d833b502655a88808665a52226'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack11.0.3000.0x64.msi'
-  checksum64    = 'ff7bfd9c0a7a375d5bf6b383c4d985e630532a61174c10f5bd8d0f915084bac1'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack11.1.0000.0x64.msi'
+  checksum64    = '4a23edb45f5959ecbcdaa5e74be3e72a3f6ef4c7757a9c980ee75bc272ad6fc9'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/myfamilytree/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   filetype      = 'msi'
 
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree11.0.3.0.msi'
-  checksum      = '5c6276feb7dede0b3de5afb9ace4d3b94316eb4e85263103c977e4f6231d0c91'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree11.1.0.0.msi'
+  checksum      = '9affd88658d3a944dfb590231a3bf97022e0d0d57234971dc09e1a1f09128d53'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree11.0.3.0x64.msi'
-  checksum64    = '892425da69b0a6f145afb27b360cb138d86fe65756d3914c4959699b261febff'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree11.1.0.0x64.msi'
+  checksum64    = 'b97780769e50089d801d2bb2a4fd1ac5ec59d4aa46070f36ebf72e2d6fc4e5a8'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
