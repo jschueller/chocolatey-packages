251955708d374258e9020a1067d4f6f90e7fc85f
Chocolatey <chocolatey@realdimensions.net>
2021-10-21 16:25:48 +0000

    AU: 2 updated - webex yara [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/506dd7cb4e11c9151039b146ffb2d0a60a230c19

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = '31E5ADFCA51588152741DDD68CCC9009E7AFB256B797DCCCAC01A9F1CF6955FE'
+  checksum      = '54664589F7566824BAF9C5052F3F92A9693F989562F9EEDF151E3D5E2604E071'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = 'DD9A368687748D5E28D58E532986B17C2532C7839315CA492824BCF6E8AF8158'
+  checksum64    = 'CD217970FD130EDCED0F4DD4FBA88483D1D347EAFCA44D5C39FA27B9E9AF77A6'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
--- automatic/yara/tools/chocolateyinstall.ps1
+++ automatic/yara/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\yara-v4.1.2-1693-win32.zip"
-  file64      = "$toolsDir\yara-v4.1.2-1693-win64.zip"
+  file        = "$toolsDir\yara-v4.1.3-1755-win32.zip"
+  file64      = "$toolsDir\yara-v4.1.3-1755-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
