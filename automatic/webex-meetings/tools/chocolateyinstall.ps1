6420e07c39859b93f4f3583a267871cbd7f1e47f
Chocolatey <chocolatey@realdimensions.net>
2021-11-02 00:24:38 +0000

    AU: 5 updated - apo-ok apo-ok.install apo-ok.portable cudatext webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d740be7f4db86d0ba6269fe96e593708602f4fc8

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = 'dc685c7aef3b3c23fe72e27be900a210c5d8242c1c4bf0359e883b4561590a4c58b59fce32eb1233f04de7740cf5f005cda19375ad04eba2a6aaa5030cf93178'  
+  checksum     = '4b4880d5beff5f8d098a4aa7347c85e7f36a9a1ea91d92aacfd2cc7ee56bfb7fa4fe198b704732d671c2b444111d3ba4f3e039f24952e318c6a77cce1aae6feb'  
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
