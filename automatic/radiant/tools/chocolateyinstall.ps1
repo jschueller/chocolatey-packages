55ab93d66f187b9bed97f518063334a55dbeb720
Chocolatey <chocolatey@realdimensions.net>
2021-11-04 16:25:59 +0000

    AU: 6 updated - ospray ospray.install ospray.portable radiant sonarlint-vs2017 voicemeeter-banana.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5f8fbe1807ed341e070147a1d95fe2ed646372c6

--- automatic/radiant/tools/chocolateyinstall.ps1
+++ automatic/radiant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.radiantviewer.com/files/RadiAnt-2020.2.3-Setup.exe'
-  checksum      = '8dac1a658cc090315ad2522d075689cc0faa0c30d85b53ad28d972440c1a6f8f'
+  url           = 'https://www.radiantviewer.com/files/RadiAnt-2021.2-Setup.exe'
+  checksum      = '66ace6592ac9d22a7ad1d9bf209bba37797cfc935f5082933b00431a41ff1a2c'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
