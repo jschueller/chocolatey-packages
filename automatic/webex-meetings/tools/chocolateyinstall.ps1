3f32b0e205b1902721d183647a18cebd2d03e106
Chocolatey <chocolatey@realdimensions.net>
2021-11-09 00:25:26 +0000

    AU: 2 updated - webex-meetings wireguard [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6bbaae15f39162474441f388b87317c05f2e1a09

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '4b4880d5beff5f8d098a4aa7347c85e7f36a9a1ea91d92aacfd2cc7ee56bfb7fa4fe198b704732d671c2b444111d3ba4f3e039f24952e318c6a77cce1aae6feb'  
+  checksum     = '12a7b1462092f82f789b68e933315cf06da462b36d5793f8b086cb05d65d5409c42a822b409cf927ed9257a5df3dfe2abc4175ce61ffbf3bafe4e09aaf40f4ab'  
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
