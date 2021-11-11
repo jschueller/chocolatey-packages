cae9d1f76ccc07d26e0e1d0f9fd6d38829721244
Chocolatey <chocolatey@realdimensions.net>
2021-11-11 13:36:57 +0000

    AU: 1 updated - smtube [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d0ce97101fc50fba9928a4ec8235f31c0ef046da

--- automatic/smtube/tools/chocolateyinstall.ps1
+++ automatic/smtube/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\smtube-21.7.0-win32.exe"
-  file64      = "$toolsDir\smtube-21.7.0-x64.exe"
+  file        = "$toolsDir\smtube-21.10.0-win32-qt5.6.exe"
+  file64      = "$toolsDir\smtube-21.10.0-x64.exe"
   silentArgs  = "/S"
 }
 
