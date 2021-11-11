dbd5ba7b6ad5fd22c88398b79a9a0adc1e20cfee
chtof <chtof62@gmail.com>
2021-11-11 14:37:54 +0100

    Update update.ps1

--- automatic/orfeo-toolbox/update.ps1
+++ automatic/orfeo-toolbox/update.ps1
@@ -2,30 +2,26 @@
 import-module au
 
 function global:au_GetLatest {
-     $releases = 'https://www.orfeo-toolbox.org/download/'
-     $regex32  = 'OTB-([\d\.]+)-Win32.zip'
+     $releases = 'https://www.orfeo-toolbox.org/download/'     
 	 $regex64  = 'OTB-(?<Version>[\d\.]+)-Win64.zip'
 
-	 $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $url32         = $download_page.links | ? href -match $regex32
+	 $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
      $url64         = $download_page.links | ? href -match $regex64
 
      return @{
-        Version = $matches.Version
-        URL32   = $url32.href
+        Version = $matches.Version        
         URL64   = $url64.href
     }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        "tools\chocolateyinstall.ps1" = @{		
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"            
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"            
+            "(^(\s)*[$]version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"              
         }
     }
 }
 
-update
+update -ChecksumFor 64
