0448b2eff8838e11d598eae4eee61cc94403ab8c
chtof <chtof62@gmail.com>
2021-11-11 13:43:03 +0100

    Update update.ps1

--- automatic/virtualhere-server/update.ps1
+++ automatic/virtualhere-server/update.ps1
@@ -1,8 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+function global:au_BeforeUpdate() {    
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
@@ -13,21 +12,18 @@ function global:au_GetLatest {
     (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
 
     return @{
-        Version = $matches.Version
-        URL32   = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin32.exe'
+        Version = $matches.Version        
         URL64   = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
     }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        "tools\chocolateyinstall.ps1" = @{			
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"            
             "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 64
