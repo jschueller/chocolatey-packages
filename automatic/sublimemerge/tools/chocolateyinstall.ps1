33da5e8ecbfd4140fc1620e03dab77911c800ea2
chtof <chtof62@gmail.com>
2021-11-11 14:27:28 +0100

    Update chocolateyinstall.ps1

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://download.sublimetext.com/sublime_merge_build_2059_x64_setup.exe'
-  checksum      = '7f3543dff1ace793ac5800ccfacbfe9d95f6a22a74f5893a255bfc7f4f173ef3'
+  url64         = 'https://download.sublimetext.com/sublime_merge_build_2059_x64_setup.exe'
+  checksum64    = '7f3543dff1ace793ac5800ccfacbfe9d95f6a22a74f5893a255bfc7f4f173ef3'
   checksumType  = 'sha256'
   
   
