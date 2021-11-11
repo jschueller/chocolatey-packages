71d0caead27a7c631afa7d4f34b8d7a6b0b9fa5c
chtof <chtof62@gmail.com>
2021-11-11 13:39:33 +0100

    Update update.ps1

--- automatic/winlog32/update.ps1
+++ automatic/winlog32/update.ps1
@@ -1,16 +1,23 @@
 ﻿import-module au
-$releases = 'http://www.winlog32.co.uk/dl_wl32update.htm'
-$url = 'http://www.winlog32.co.uk/files/'
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = 'v(.*?) \(rev\.(\d+)\)'
-	 $download_page -match $regex
-	 $version_url = $matches[1].replace('.','')
-     $version = $matches[1] + "." + $matches[2]
-     return @{ Version = $version ; URL32 = $url + "wl32v" + $version_url + ".exe" }
+    $releases = 'http://www.winlog32.co.uk/dl_wl32update.htm'
+    $url = 'http://www.winlog32.co.uk'
+    #$regex   = 'v(.*?) \(rev\.(\d+)\)'
+    $regex   = 'v([\d.]+)(\r\n| \(rev\.(\d+)\))'
+    $regex_url = '/files/wl32v[\d]+.exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing    
+    $download_page -match $regex
+    $version_url = $matches[1].replace('.','')
+    #$version = $matches[1] + "." + $matches[2]
+    $version = $matches[1]
+    $download_page -match $regex_url
+    $url_path = $matches[0]
+
+    return @{ Version = $version ; URL32 = $url + $matches[0] }
 }
 
 function global:au_SearchReplace {
