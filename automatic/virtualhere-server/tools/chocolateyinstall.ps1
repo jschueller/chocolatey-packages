80f97a0ba24d0420bc603e04645d27d3ea82b1b0
chtof <chtof62@gmail.com>
2021-11-11 13:42:58 +0100

    Update chocolateyinstall.ps1

--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -5,10 +5,6 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
-  url            = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin32.exe'
-  checksum       = 'ab9564ff674976d002d6d586ad490f7b82de92fd238a7c99aa790ee70e648a66'
-  checksumType   = 'sha256'
-
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
   checksum64     = '07af6ae82e2fe73340b666c203f591bfa32c4c6d8c7603e83e8e97afccf9865f'
   checksumType64 = 'sha256'
