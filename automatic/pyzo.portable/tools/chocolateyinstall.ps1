b428e2f074aa47b3de1f7e203128dd2710d0d5d7
Chocolatey <chocolatey@realdimensions.net>
2021-11-25 00:24:21 +0000

    AU: 3 updated - pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fb5cf0825b977565f399040433be9e9918c33bb5

--- automatic/pyzo.portable/tools/chocolateyinstall.ps1
+++ automatic/pyzo.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file32      = "$toolsDir\pyzo-4.11.2-win32.zip"
-  file64      = "$toolsDir\pyzo-4.11.2-win64.zip"
+  file32      = "$toolsDir\pyzo-4.11.4-win32.zip"
+  file64      = "$toolsDir\pyzo-4.11.4-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -14,5 +14,5 @@ Remove-Item -Path $packageArgs.file64
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Pyzo.lnk"
-$targetPath = Join-Path $toolsDir "pyzo-4.11.2\pyzo.exe"
+$targetPath = Join-Path $toolsDir "pyzo-4.11.4\pyzo.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
