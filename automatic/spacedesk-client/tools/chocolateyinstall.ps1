eeac2bb6b2d87ce13f457d9bd269d73deef5f402
Chocolatey <chocolatey@realdimensions.net>
2021-12-10 16:24:46 +0000

    AU: 4 updated - cudatext sd spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/43c0850a5d1613748eb2bcd1826916f5bd64440f

--- automatic/spacedesk-client/tools/chocolateyinstall.ps1
+++ automatic/spacedesk-client/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'
 
-  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0934_BETA.msi'
-  checksum      = 'd354c548b94146292b206791dccc02df1e71a37e3970cbd7000172cac3626d02'
+  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0935_BETA.msi'
+  checksum      = 'e81be9fcd1e2886921d673e4e601bddb1d2ea0e0b4fc9008c5064273953cbf94'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
