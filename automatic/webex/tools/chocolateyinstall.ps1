bb8313e9d0b9e068c02b09d19d7d7777daf838a4
Chocolatey <chocolatey@realdimensions.net>
2021-12-14 00:24:56 +0000

    AU: 2 updated - chirp webex [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/51a1a990e2349701abb0406d775f96c831459e0f

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = 'A29D27B2B0BC65DF70A716E4D5F5F59E439B2CE7295302590C2E9B3389531B92'
+  checksum      = '1463EC4F71E632A2A794C1DC4B3510791F1295DA5D5C5510DCF4CAEF7CA68D0F'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = '9E3E83C96991819ECD68046310D87575B29A539C57665E732E8FC344E9BE6C28'
+  checksum64    = '807F49378FCF31202B2ABC72C9AAA87EEF66E4B8BB10C83156EC68BACBDF445A'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
