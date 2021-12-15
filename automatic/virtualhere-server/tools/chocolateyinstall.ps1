4ff2bca8b065ad3aed91afd67762e3dbb1d661d2
Chocolatey <chocolatey@realdimensions.net>
2021-12-15 08:24:35 +0000

    AU: 3 updated - cudatext gns3 virtualhere-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/bf9c926cf7957c93c6a0c6967b96846ee522cf95

--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
-  checksum64     = '77645c3cfa7aba5b87d49a1fd2d30986696745da299d9dba732ced261524fd0b'
+  checksum64     = 'e142d3275e509f67555fc7df7a211164b34678ab6d2e6009c289887ba0efc947'
   checksumType64 = 'sha256'
 }
 
