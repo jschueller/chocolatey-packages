6cf82d274e38de4d45f3c1e4184a098aaee7d62b
Dominik S <dominik@schmelz.cc>
2021-12-28 11:27:35 +0100

    Update chocolateyinstall.ps1
    Fix error hashes do not match

--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.18.1/TIB_js-studiocomm_6.18.1_windows_x86_64.exe'
-  checksum64     = '3327ca492684456cdcf3a27d1f9c7df9456cf31865e6589a6c1744f9b8ed6985'
+  checksum64     = '7b7777793a1ebd144c6df07d03d3a2bee6e38b02d4ecca28109bafec58de6921'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
