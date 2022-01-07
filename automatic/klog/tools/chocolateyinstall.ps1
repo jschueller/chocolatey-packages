40c07b564997dc5d10e4fbd506537ea795ba3962
Chocolatey <chocolatey@realdimensions.net>
2022-01-07 19:16:14 +0000

    AU: 2 updated - cudatext klog [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/4f3c993b69c91c88e1cc732c8e23e50d7e2b2d13

--- automatic/klog/tools/chocolateyinstall.ps1
+++ automatic/klog/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'KLog'
   fileType      = 'exe'
-  file          = "$ToolsDir\KLog-1.8.5-windows-installer.exe"
+  file          = "$ToolsDir\KLog-1.8.6-windows-installer.exe"
   silentArgs    = "--mode unattended"
 }
 
