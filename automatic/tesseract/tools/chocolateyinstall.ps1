0c58c5d1389c6bd565c34cbfc71a14a3342e08cb
Chocolatey <chocolatey@realdimensions.net>
2022-01-08 16:25:18 +0000

    AU: 2 updated - gns3 tesseract [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/159263370efc1f9b4086cb1ca33caac6f1e40bbc

--- automatic/tesseract/tools/chocolateyinstall.ps1
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.0.0-rc1.20211030.exe"
-  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.0.0-rc1.20211030.exe"
+  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.0.1.20220107.exe"
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.0.1.20220107.exe"
   silentArgs  = '/S'
 }
 
