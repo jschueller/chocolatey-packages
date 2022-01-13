2cdba33e780ef362a46405292c9073b5a544dda1
Chocolatey <chocolatey@realdimensions.net>
2022-01-13 08:34:19 +0000

    AU: 2 updated - dacia-toolbox spacedesk-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/c7865b3047ccb405a2059f49b26937b376cdadf6

--- automatic/dacia-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-toolbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2021_12_14_15_31/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
-  checksum     = 'eba9e6f127e48d6ced0c6dd0851add5b197282ecdba70265ad443925502e6cff'
+  url          = 'https://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2022_01_05_10_59/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
+  checksum     = 'a3f62456394bc9089398838356f1f1d7dbd018d1a6ec6461aa545e1afd874ea0'
   checksumType = 'sha256'
 
   silentArgs   = "/S"
