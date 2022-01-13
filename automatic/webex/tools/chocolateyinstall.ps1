ecd1fa667728fcc3a7fce4ff7e9bce0fbb409ea0
Chocolatey <chocolatey@realdimensions.net>
2022-01-13 16:26:31 +0000

    AU: 2 updated - ffmpeg-batch webex [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/73eedf69ff95f9e84bcbb926e37092dd150b7181

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = '1463EC4F71E632A2A794C1DC4B3510791F1295DA5D5C5510DCF4CAEF7CA68D0F'
+  checksum      = '8D010FC39944C68681467FB455E2371007703A637FCD6CE3AB9A1F5393B7F534'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = '807F49378FCF31202B2ABC72C9AAA87EEF66E4B8BB10C83156EC68BACBDF445A'
+  checksum64    = 'EFD25E92F296220A070DB23AB09C4EFB22CC15F89F46861659FE9675AD698B10'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
