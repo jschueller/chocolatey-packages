9de3a4841f9998aa959be84a8a7e403444a0a389
Chocolatey <chocolatey@realdimensions.net>
2022-01-19 08:25:34 +0000

    AU: 3 updated - moonlight-qt moonlight-qt.install moonlight-qt.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/1b9769145f626e93e59417bccacffd73bd990253

--- automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\MoonlightSetup-3.1.4.exe"  
+  file          = "$toolsDir\MoonlightSetup-3.2.0.exe"  
   silentArgs	= '/S'
 }
 
