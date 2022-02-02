e1b5906af9000b9faf9f5b6e78bdfddab5727271
Chocolatey <chocolatey@realdimensions.net>
2022-02-02 08:25:20 +0000

    AU: 2 updated - spacedesk-client webex [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2b235eea643751e092535334670382c387ec6dd3

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = '8D010FC39944C68681467FB455E2371007703A637FCD6CE3AB9A1F5393B7F534'
+  checksum      = '06A7CC71F9C9E12784CE3C84475A91D6BE6319B7D4840B0CED4DF6F601B1A165'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = 'EFD25E92F296220A070DB23AB09C4EFB22CC15F89F46861659FE9675AD698B10'
+  checksum64    = 'F09771F0D92CAF2057BA513B147833A514451A50F467837FAFD2CCFAF756098F'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
