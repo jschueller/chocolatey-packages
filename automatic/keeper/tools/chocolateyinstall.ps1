17aa16a84acd398987f1244c0a324c6fb1c2386a
Chocolatey <chocolatey@realdimensions.net>
2022-02-04 08:25:38 +0000

    AU: 2 updated - keeper numpy [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/67e95a32a6bb9955bb69a16a8462a160f2d1544d

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = 'b67d2a3efe35b091b8a49c15173034efa094b2982fc116f619b34634eed1c9c9'
+  checksum      = '6bfc0a948925cf40320ee6b770ca59f5a5b18cae307d84f585cf980e64dd9ac4'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'dfdba9aab734aae9660aa6850ecf02df131540ac9140d84176d445f1e5cd98da'
+  checksum64    = '7105714d9f6322ad810357f9f535bb814e20e1406ebe51376fbb871f3677b5f9'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/numpy/tools/chocolateyinstall.ps1
+++ automatic/numpy/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿Update-SessionEnvironment
-$version = '1.22.1'
+$version = '1.22.2'
  
 $proxy = Get-EffectiveProxy
 if ($proxy) {
