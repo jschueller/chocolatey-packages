3ef31c91159487ec97e4fd80e73406010765af4b
Chocolatey <chocolatey@realdimensions.net>
2022-02-05 08:25:33 +0000

    AU: 4 updated - dontsleep dontsleep.install dontsleep.portable keeper [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f3b475ef0c7fdc595939d2b770af5a5f4e533ec7

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '6bfc0a948925cf40320ee6b770ca59f5a5b18cae307d84f585cf980e64dd9ac4'
+  checksum      = '3cc93429aba0246878fc627056f043aa4b67782dd63a55038e665cfc882f8cbe'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '7105714d9f6322ad810357f9f535bb814e20e1406ebe51376fbb871f3677b5f9'
+  checksum64    = '9b6cd5b49a8c43231028c67f9ff5eb840e561bec55c879e30327b2e67488b3c3'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
