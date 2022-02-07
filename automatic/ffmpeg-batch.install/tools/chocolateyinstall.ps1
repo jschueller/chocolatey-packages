a86db32cc6977c4bd4050ccf08f59e71adc7627e
Chocolatey <chocolatey@realdimensions.net>
2022-02-07 00:25:29 +0000

    AU: 3 updated - ffmpeg-batch ffmpeg-batch.install ffmpeg-batch.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/91e0f1c8bdab799b4503e913261f87ab5e800bdc

--- automatic/ffmpeg-batch.install/tools/chocolateyinstall.ps1
+++ automatic/ffmpeg-batch.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\FFBatch_AV_Converter_2.7.3_32bit.exe"
-  file64      = "$toolsDir\FFBatch_AV_Converter_2.7.3_64bit.exe"
+  file        = "$toolsDir\FFBatch_AV_Converter_2.7.4_32bit.exe"
+  file64      = "$toolsDir\FFBatch_AV_Converter_2.7.4_64bit.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
