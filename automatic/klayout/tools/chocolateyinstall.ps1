c4d03931a8dc6c7caa6f5d2f0e4df2fb921c4357
Chocolatey <chocolatey@realdimensions.net>
2022-02-14 16:25:58 +0000

    AU: 4 updated - klayout log4om log4om.install log4om.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/98adaad7b659e4e2e07efb822ac4eddb7c4daa4a

--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.27.7-win32-install.exe'
-  checksum      = '657e6db058b1e2548c67a0367f34ab517757e3c6a30b835743e6ad0c9157823f'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.27.8-win32-install.exe'
+  checksum      = '8573b36f623db7a322f8a182a3e0a4e5ac786c9e3fcd04b0559e096d4e169d5b'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.27.7-win64-install.exe'
-  checksum64    = 'be7d5eed482588be195c0b76fc839f40bd409141efa1c7cef99f18596b837320'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.27.8-win64-install.exe'
+  checksum64    = '0eade674ed148d631916ad73b739eb139d12824df2f74f24a3fce3a4dae43de3'
 
   checksumType64= 'sha256'
 
