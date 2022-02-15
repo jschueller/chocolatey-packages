d5c1fb4dee8fcba1ee64f0d27fe9055804b68c11
Chocolatey <chocolatey@realdimensions.net>
2022-02-15 20:39:55 +0000

    AU: 1 updated - glow [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/3babfbcceed7470963594381c3ec9264bcb7ce07

--- automatic/glow/tools/chocolateyinstall.ps1
+++ automatic/glow/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\glow_1.4.0_Windows_i386.zip"
-  file64        = "$toolsDir\glow_1.4.0_Windows_x86_64.zip"
+  file          = "$toolsDir\glow_1.4.1_Windows_i386.zip"
+  file64        = "$toolsDir\glow_1.4.1_Windows_x86_64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
