9f0bb2a91110b9a78598791ae37c5c5f3cebf669
chtof <chtof62@gmail.com>
2022-02-17 19:53:25 +0100

    Update chocolateyInstall.ps1

--- automatic/vuescan/tools/chocolateyInstall.ps1
+++ automatic/vuescan/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url            = 'https://www.hamrick.com/files/vuex3297.exe'
-  checksum       = '4b375f15aa5756f3ca60844eabb69b8fb74bdf34200086ea30d260c633e87c9d'
+  checksum       = 'de6f64771228de1a6f48a0e7560121251c3b2facd7fb8d4856c1d8c6fc1c11f1'
   checksumType   = 'sha256'
 
   url64          = 'https://www.hamrick.com/files/vuex6497.exe'
-  checksum64     = '55aa5b2678c953a7bb6ef5b1b9addebf56058d6273b19668fc2aa544383ee10b'
+  checksum64     = '44fed6a4e653a1a0ba194b5eb293d3f98a770ca1f23e0e0a11f645871e9b16f9'
   checksumType64 = 'sha256'
 }
 
