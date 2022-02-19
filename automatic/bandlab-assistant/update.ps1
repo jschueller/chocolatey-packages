cd45c6cc29e5e0e343f00c3c532ac064f5b94578
chtof <chtof62@gmail.com>
2022-02-19 11:54:18 +0100

    Update update.ps1

--- automatic/bandlab-assistant/update.ps1
+++ automatic/bandlab-assistant/update.ps1
@@ -1,65 +1,34 @@
 $ErrorActionPreference = 'Stop'
 Import-Module au
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
-function global:au_GetLatest {
-    $downloadUrl = 'https://www.bandlab.com/download/assistant/windows'
-    $etag = GetETagIfChanged -uri $downloadUrl    
+function global:au_GetLatest {    
+    $downloadUrl = 'https://static.bandlab.com/assistant/latest.yml'
 
-    If ($etag) {    
-        $result = GetResultInformation $downloadUrl
-        $result["ETAG"] = $etag
-    } Else {    
-        $result = @{
-            URL32   = $downloadUrl
-            Version = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
-        }
+    $regexVersion = 'version: (?<Version>[\d\.]+)'
+    $regexFile = 'path: (?<File>.*)'    
+
+    $download_Page = Invoke-WebRequest -Uri $downloadUrl -UseBasicParsing
+
+    $download_Page -match $regexVersion    
+    $version = $matches.Version
+
+    $download_Page -match $regexFile
+    $file = $matches.File
+
+    return @{
+      Version = $version
+      URL32   = 'https://static.bandlab.com/assistant/' + [uri]::EscapeDataString("$file")            
     }
-    return $result
+    
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
             "(^(\s)*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"            
         }
     }
 }
 
-function global:au_AfterUpdate {
-  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-}
-
-function GetResultInformation([string]$url32) {
-  $dest = "$env:TEMP\bandlab-assistant-windows-latest.exe"
-  Get-WebFile $url32 $dest | Out-Null
-
-  $result = @{
-    URL32          = $url32
-    Version        = (Get-Item $dest).VersionInfo.ProductVersion.Trim()
-    Checksum       = Get-FileHash $dest -Algorithm SHA512 | % Hash
-    ChecksumType32 = 'sha512'
-  }
-  Remove-Item -Force $dest
-  return $result
-}
-
-function GetETagIfChanged() {
-  param([string]$uri)
-  If (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
-    $existingETag = $etag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -first 1
-  }
-  Else { $existingETag = $null }
-
-  $etag = Invoke-WebRequest -Method Head -Uri $uri -UseBasicParsing
-  $etag = $etag | % { $_.Headers.ETag }
-  If ($etag -eq $existingETag) { return $null }
-
-  return $etag
-}
-
-If ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update
-}
+update
