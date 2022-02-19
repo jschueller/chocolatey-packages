b11e0c61709a45a261d776e68fecd2b27de94f0e
Chocolatey <chocolatey@realdimensions.net>
2022-02-19 08:23:44 +0000

    AU: 4 updated - chirp chirp.install chirp.portable mboxviewer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7168f1d808ad92e75639842de376a91377410041

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20220202-installer.exe"
+  file          = "$toolsDir\chirp-daily-20220219-installer.exe"
   silentArgs    = '/S'
 }
 
