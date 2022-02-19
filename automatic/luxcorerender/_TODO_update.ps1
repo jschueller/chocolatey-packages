1b6cc90cc22591dbd359848c79fc5779129706ba
chtof <chtof62@gmail.com>
2022-02-19 12:08:58 +0100

    Create _TODO_update.ps1

---
+++ automatic/luxcorerender/_TODO_update.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://luxcorerender.org/download'
+    $regex    = 'luxcorerender-v(?<Version>[\d\.]+)-win64.zip'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version ; URL64 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(Join-Path [$]toolsDir `"luxcorerender-v)[\d\.]+(-win64\\luxcoreui.exe`")" = "`${1}$($Latest.Version)`${2}"
+        }        
+    }
+}
+
+
+try {
+    update -ChecksumFor 64
+} catch {
+    $ignore = 'The request was aborted: Could not create SSL/TLS secure channel.'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
