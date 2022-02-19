ccd49e6284a63c48e67282b7995b292126ff7e68
chtof <chtof62@gmail.com>
2022-02-19 20:18:36 +0100

    Create _update.ps1

---
+++ automatic/z-cron/_update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.z-cron.com/download.html'
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
+	$regex   = '<td class="boxe">(?<Version>[\d\.]+) Build (?<Build>\d+) \('
+    $regex   = '</strong> (?<Version>[\d\.]+) Build (?<Build>\d+)</p>'
+
+	$download_page.Content -match $regex | Out-Null
+	if ($matches.Build) {
+	   $version = $matches.Version + "." + $matches.Build
+	} else {
+	   $version = $matches.Version
+	}
+	return @{
+        Version = $version
+        URL32   = 'https://www.z-download.de/down/zcron.zip'        
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update -checksumfor none
