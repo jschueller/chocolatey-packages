eba6a94a6d8959942fb638a111f9c18d3676bef3
chtof <chtof62@gmail.com>
2022-02-19 20:02:00 +0100

    Update chocolateyinstall.ps1

--- automatic/clavier-plus.portable/tools/chocolateyinstall.ps1
+++ automatic/clavier-plus.portable/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip @packageArgs
-Remove-Item -Path $packageArgs.file,$packageArgs.file64
+Remove-Item -Path $packageArgs.file64
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
