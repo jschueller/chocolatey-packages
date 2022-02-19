17c13a2611294485229e9c6eb1e77e715e7b3510
Chocolatey <chocolatey@realdimensions.net>
2022-02-19 17:50:09 +0000

    AU: 1 updated - spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/3810e30b3b39986df85079fab6c57e3a72b02ab6

--- automatic/spacedesk-server/tools/chocolateyinstall.ps1
+++ automatic/spacedesk-server/tools/chocolateyinstall.ps1
@@ -1,23 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-  $url32_win10      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_10_32_v1025_BETA.msi'
+  $url32_win10      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_10_32_v1036_BETA.msi'
   $checksum32_win10 = ''
   $url32            = $url32_win10
   $checksum32       = $checksum32_win10
-  $url64_win10      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_10_64_v1025_BETA.msi'
+  $url64_win10      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_10_64_v1036_BETA.msi'
   $checksum64_win10 = ''
   $url64            = $url64_win10
   $checksum64       = $checksum64_win10
 } elseif ( ( [environment]::OSVersion.Version.Major -ge 7 ) -or
            ( ( [environment]::OSVersion.Version.Major -eq 6 ) -and
              ( [environment]::OSVersion.Version.Minor -eq 1 ) ) ) {
-  $url32_win7_81      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_32_v1020_BETA.msi'
-  $checksum32_win7_81 = 'bf1ebdd47c86cf08a0ad2eb236dd6fb1a5e90989268b5cdbd38ffdbca70a66e3'
+  $url32_win7_81      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_32_v1036_BETA.msi'
+  $checksum32_win7_81 = 'f016b1d6ba56bdadc185ac47c83e439bac2cb89100a21891ad42429b69509fed'
   $url32              = $url32_win7_81
   $checksum32         = $checksum32_win7_81
-  $url64_win7_81      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_64_v1020_BETA.msi'
-  $checksum64_win7_81 = '4ee3ab8cf0ac8797d658821ffea153ecb6dee6625f0e4a3dd1d2cdb90462dbf6'
+  $url64_win7_81      = 'https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_64_v1036_BETA.msi'
+  $checksum64_win7_81 = '97da63b8cca9b494b4880be74dd540e23da1906118ccdb5e5564d35005b34fdc'
   $url64              = $url64_win7_81
   $checksum64         = $checksum64_win7_81  
 } else {
