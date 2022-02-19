ab681d525e74e9e3e3730b8238d0ce49524fdb6f
Chocolatey <chocolatey@realdimensions.net>
2022-02-19 12:24:42 +0000

    AU: 1 updated - qrq [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b2668291f5f7ff5feb37adadb2e8137af83d430e

--- automatic/qrq/tools/chocolateyinstall.ps1
+++ automatic/qrq/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\qrq-0.3.3.exe"
+  file        = "$toolsDir\qrq-0.3.5.exe"
   silentArgs  = '/S'
 }
 
