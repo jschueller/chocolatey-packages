2da650bd5df929f5c51279301111beadc68b0ef0
chtof <chtof62@gmail.com>
2022-02-19 20:09:54 +0100

    Update chocolateyinstall.ps1

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.0.exe'
-  checksum      = '4729bdd35d3913200578152a8d4d47f6eef2f71af0b0fc26d19ff79c56e1c26d'
+  checksum      = '6f84887637ef99e2de75891506c668089e0b71d86c3b6f9d6bd14716b35645171b2b5d23941101e868a7a02a865bddd136d4d15864607b219ba126fc0141cc0f'
   checksumType  = 'sha512'
 
   silentArgs    = '/S'
