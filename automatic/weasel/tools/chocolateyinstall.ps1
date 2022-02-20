87467963889d5d012b20f30cd1435fe0495914d7
chtof <chtof62@gmail.com>
2022-02-20 12:39:36 +0100

    Update chocolateyinstall.ps1

--- automatic/weasel/tools/chocolateyinstall.ps1
+++ automatic/weasel/tools/chocolateyinstall.ps1
@@ -1,10 +1,16 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$silentArgs = '/S'
+
+$pp = Get-PackageParameters
+
+# /T - install as a Traditional Chinese IME (zh_TW locale)
+if ($pp['T'] -eq 'true') { $silentArgs += ' /T' }
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = "$toolsDir\weasel-0.14.3.0-installer.exe"
-  silentArgs  = "/S"
+  silentArgs  = "$silentArgs"
 }
 
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
