6877b05686f2a6638fee1c44b508fd6e5f84a99d
chtof <chtof62@gmail.com>
2022-03-12 12:59:54 +0100

    Update chocolateyinstall.ps1

--- automatic/marktext.portable/tools/chocolateyinstall.ps1
+++ automatic/marktext.portable/tools/chocolateyinstall.ps1
@@ -2,14 +2,17 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  destination = "$toolsDir"
-  file        = "$toolsDir\marktext-ia32-win.zip"
-  file64      = "$toolsDir\marktext-x64-win.zip"
-}
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$toolsDir"
 
-Get-ChocolateyUnzip @packageArgs
-Remove-Item -Path $packageArgs.file,$packageArgs.file64
+  url32        = 'https://github.com/marktext/marktext/releases/download/v0.17.1/marktext-ia32-win.zip'
+  checksum32   = ''
+  url64        = 'https://github.com/marktext/marktext/releases/download/v0.17.1/marktext-x64-win.zip' 
+  checksum64   = ''
+
+  checksumType = 'sha256'  
+}
+Install-ChocolateyZipPackage @packageArgs
 
 # Install start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
