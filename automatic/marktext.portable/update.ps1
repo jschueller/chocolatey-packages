b34a666fa720bf703dfb5c8227e6504ccc8e1229
chtof <chtof62@gmail.com>
2022-03-12 13:00:08 +0100

    Update update.ps1

--- automatic/marktext.portable/update.ps1
+++ automatic/marktext.portable/update.ps1
@@ -1,10 +1,13 @@
 ﻿import-module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate() {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+  $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
 
 function global:au_GetLatest {	
-    $github_repository = "marktext/marktext"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $github_repository = 'marktext/marktext'
+    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
     $regex32  = '/.*-ia32-win.zip'
     $regex64  = '/v(?<Version>[\d\.]*)/.*-x64-win.zip'
 
@@ -21,20 +24,15 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64).*"               = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
-        }
-
-        #"tools\chocolateyinstall.ps1" = @{			
-        #    "(marktext-)[\d\.]+(-\w+-win.zip)" = "`${1}$($Latest.Version)`${2}"
-        #}
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url32\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`${1}'$($Latest.URL64)'"
+            "(^(\s)*checksum32\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
+        }        
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor none -noCheckUrl
 }
