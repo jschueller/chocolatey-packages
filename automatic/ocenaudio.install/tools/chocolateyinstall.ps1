6a2f72aaaaf353e9b8ca9d09c7961eefccba6b6f
Chocolatey <chocolatey@realdimensions.net>
2022-03-15 00:25:51 +0000

    AU: 6 updated - ocenaudio ocenaudio.install ocenaudio.portable pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/07c6446309c86a5040efb76fc7df2dedcc0fb160

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
   url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio.exe'
-  checksum       = 'e47c9544669bfafa632f78b8d641486e805f34d5e4ec50e11abfa4a65b5741b8'
+  checksum       = 'e28951c33d7d154c69c84e62e1917695ebafc4f387b605fa35b1e599808c1cb7'
   checksumType   = 'sha256'
 
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64.exe'
-  checksum64     = '42d6f07d615272777f29340dc78c815582b3e4f85364024ba42e32e1dd9eddf9'
+  checksum64     = '50d2dd5cf6c3f2985b5bf54a0906c3200e2fcb4832cb73781c506fb6299bcd4c'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
