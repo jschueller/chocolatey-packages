cc1777f7f373dd400466c302d26a5e0d1672830c
chtof <chtof62@gmail.com>
2022-03-17 00:37:04 +0100

    Update update.ps1

--- automatic/plantronicshub/update.ps1
+++ automatic/plantronicshub/update.ps1
@@ -1,4 +1,6 @@
-import-module au
+$ErrorActionPreference = 'Stop'
+Import-Module au
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
@@ -8,15 +10,23 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-  $releases = 'https://www.poly.com/us/en/support/downloads-apps/hub-desktop'
-  $regex    = 'Version (?<Version>[\d\.]+)<'
+  # $releases = 'https://www.poly.com/us/en/support/downloads-apps/hub-desktop'
+  # $regex    = 'Version (?<Version>[\d\.]+)<'
 
-  (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regex | Out-Null
+  # (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regex | Out-Null
 
-	return @{
-    Version = $matches.Version
-    URL32   = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
-  }
+  $etag = GetETagIfChanged -uri "https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe"
+
+    if ($etag) {        
+        $result = GetResultInformation "https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe"
+        $result["ETAG"] = $etag
+    }
+    else {        
+        $result = @{
+            URL32   = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
+            Version = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
+        }
+    }	
 }
 
 function global:au_SearchReplace {
@@ -28,4 +38,36 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor none
+function global:au_AfterUpdate {
+  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
+}
+
+function GetResultInformation([string]$url32) {
+  $dest = "$env:TEMP\PlantronicsHubInstaller.exe"
+  Get-WebFile $url32 $dest | Out-Null
+
+  $result = @{
+    URL32          = $url32    
+    Version        = (Get-Item $dest).VersionInfo.FileVersion.Trim()
+    Checksum       = Get-FileHash $dest -Algorithm SHA512 | % Hash    
+    ChecksumType32 = 'sha512'
+  }  
+  Remove-Item -Force $dest
+  return $result
+}
+
+function GetETagIfChanged() {
+  param([string]$uri)
+  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
+    $existingETag = $etag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -first 1
+  }
+  else { $existingETag = $null }
+
+  $etag = Invoke-WebRequest -Method Head -Uri $uri -UseBasicParsing
+  $etag = $etag | % { $_.Headers.ETag }
+  if ($etag -eq $existingETag) { return $null }
+
+  return $etag
+}
+
+update -ChecksumFor none -NoCheckUrl
