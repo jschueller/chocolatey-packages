da4c8e9699ede8b710574dc145707ee28a4f1a5f
chtof <chtof62@gmail.com>
2022-03-17 00:01:53 +0100

    Update chocolateyinstall.ps1

--- automatic/luxcorerender/tools/chocolateyinstall.ps1
+++ automatic/luxcorerender/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  checksum64  = ''
+  unzipLocation  = "$installDir"  
   url64       = 'https://github.com/LuxCoreRender/LuxCore/releases/download/luxcorerender_v2.6/luxcorerender-v2.6-win64.zip'
+  checksum64  = '9e7f2d0a7e2cc46ff02fe8cb8d88fa82d17b6eabfc1ce87cfe22e0b8961fc902'
 }
-
 Install-ChocolateyZipPackage @packageArgs
 
 # Install start menu shortcut
