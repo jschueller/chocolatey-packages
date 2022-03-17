f0a72e77e03f65fe504a2109c7310c823e2bfb2d
chtof <chtof62@gmail.com>
2022-03-17 01:07:53 +0100

    Create update.ps1

---
+++ automatic/awscli-session-manager/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html'
+    $regexVersion = '<td>(?<Version>[\d\.]+)</td>'
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regexVersion
+	
+    return @{ Version = $matches.Version ; URL32 = 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/windows/SessionManagerPluginSetup.exe' }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }        
+    }
+}
+
+update -ChecksumFor none
