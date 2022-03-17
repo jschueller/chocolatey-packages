9948b7957f8ca89c105a70fbd94c1b54aa2490b7
chtof <chtof62@gmail.com>
2022-03-17 01:08:15 +0100

    Update chocolateyinstall.ps1

--- automatic/easyuefi/tools/chocolateyinstall.ps1
+++ automatic/easyuefi/tools/chocolateyinstall.ps1
@@ -10,4 +10,10 @@ $packageArgs = @{
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
+# Note: Installation does not work with Windows 2012 R2
+ If ( [environment]::OSVersion.Version.Major -lt 10 )  {
+   Write-Warning 'Skipping installation because EasyUEFI supports only Windows 10, Windows 2016 and Windows 2019.'
+   return
+ }
+
 Install-ChocolateyPackage @packageArgs
