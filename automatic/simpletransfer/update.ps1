9de18c24c8d44bc70ae1b763415cc996cc04511e
chtof <chtof62@gmail.com>
2022-03-22 19:34:40 +0100

    Create update.ps1

---
+++ automatic/simpletransfer/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+	$releases = 'https://rambax.com/simpletransfer/download'
+	$regex    = 'SimpleTransfer Desktop Setup (?<Version>[\d\.]+).exe'
+	
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$download_page.Content -match $regex
+	
+    return @{ Version = $matches.Version ; URL32 = 'https://rambax.com' + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
