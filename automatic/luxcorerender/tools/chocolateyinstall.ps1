712de9999c00f1adc854f170d10eb4c96f1eb07c
chtof <chtof62@gmail.com>
2022-03-25 22:40:50 +0100

    Update chocolateyinstall.ps1

--- automatic/luxcorerender/tools/chocolateyinstall.ps1
+++ automatic/luxcorerender/tools/chocolateyinstall.ps1
@@ -2,10 +2,11 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  unzipLocation  = "$toolsDir"
-  url64       = 'https://github.com/LuxCoreRender/LuxCore/releases/download/luxcorerender_v2.6/luxcorerender-v2.6-win64.zip'
-  checksum64  = '9e7f2d0a7e2cc46ff02fe8cb8d88fa82d17b6eabfc1ce87cfe22e0b8961fc902'
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+  url64         = 'https://github.com/LuxCoreRender/LuxCore/releases/download/luxcorerender_v2.6/luxcorerender-v2.6-win64.zip'
+  checksum64    = '9e7f2d0a7e2cc46ff02fe8cb8d88fa82d17b6eabfc1ce87cfe22e0b8961fc902'
+  checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
