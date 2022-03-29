24c09e57256bf30bbe8c380ca2dcae4cc4007e3a
Chocolatey <chocolatey@realdimensions.net>
2022-03-29 08:30:21 +0000

    AU: 2 updated - cudatext simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/71f6aacd09ff3e408e97f872b4b40c83136bc7ae

--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.1.3.exe'
-  checksum      = '2ad927801b6c893ed95e6176a449cc46d955c62ec74ab39779b6fdad205d0401'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.1.4.exe'
+  checksum      = 'da7a6cef29ebc51fde5f47592d9c7bfb5c1f4233d37fd5e177075027a66ce780'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
