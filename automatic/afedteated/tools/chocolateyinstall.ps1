54a5a6c088364ee78768db35a4916f35dd41df49
Chocolatey <chocolatey@realdimensions.net>
2022-04-01 08:25:37 +0000

    AU: 2 updated - afedteated simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/53401e18c62810e844701afe21099acf22d2e7c8

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '3a72ead46c12a0d748ec946737d0ffa1ec2f18b701bdb09157771d945c9794766aeb73cfe636823e8b487a9560702fdc70aaa8d4fbc2ff662ad42aab4a130289'
+  checksum     = '2ad48de373a995d6da5815c34e07eac9c5e7f589601d52f9f3231e279a2a5974313e58cef6abab397abe3efcda933975089e16e4dd106ac673cc53ca21341484'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.1.4.exe'
-  checksum      = 'da7a6cef29ebc51fde5f47592d9c7bfb5c1f4233d37fd5e177075027a66ce780'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.0.exe'
+  checksum      = 'b454230aaca06ef163a327afb63c0378382fb2f34b056c86d5801cd9bf9a37c1'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
