1033bff0d23f9b4780dd49295e212ed21f9db64b
Chocolatey <chocolatey@realdimensions.net>
2022-04-02 00:25:33 +0000

    AU: 3 updated - goodsync jami simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6cc05fdcc232fbba3d56e0e3c8c809fcc029ba01

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = 'b8a809032008cab651176b40bd876638d4d5a4577a86ed58b55fe00894e3e65a'
+  checksum       = '3849b8c48cf6b6ce18631fac8cf17f082c6ec16a93e9ce34e27145471d68584e'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.0.exe'
-  checksum      = 'b454230aaca06ef163a327afb63c0378382fb2f34b056c86d5801cd9bf9a37c1'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.1.exe'
+  checksum      = '45ec48f4fded9cd4f519cca8cc6a4335f70bb69e038079c06c8be2c570a7482a'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
