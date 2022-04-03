deab05f3324d33e13d75f5cff88601a5e215e60a
Chocolatey <chocolatey@realdimensions.net>
2022-04-03 00:25:46 +0000

    AU: 4 updated - moonlight-qt moonlight-qt.install moonlight-qt.portable simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/13f2776a16b10ce24dc33ac27abacc07bc0e50a0

--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.1.exe'
-  checksum      = '45ec48f4fded9cd4f519cca8cc6a4335f70bb69e038079c06c8be2c570a7482a'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.2.exe'
+  checksum      = '9633974ed9d4f292d76b982cfce7df1fb1c09d2647e3e7de2b5df94d9a22b5f5'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
