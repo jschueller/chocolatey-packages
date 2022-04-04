8651cb47c729aebc97efe8e40b22563efe8375be
Chocolatey <chocolatey@realdimensions.net>
2022-04-04 16:25:46 +0000

    AU: 4 updated - automouseclick geogebra6 geogebra-geometry simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/3c63eaf2a7d2f4c4d9fc91d178ac2359e4012cc6

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '989CD3A4660A21889131446A2EC86C5ADFF75E465CF9028F10C2410D722D4B71'
+  checksum     = '9932D9CD01C3230164A03FB1B66EF92CE2055CD92460478A165DA2C167608086'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.2.exe'
-  checksum      = '9633974ed9d4f292d76b982cfce7df1fb1c09d2647e3e7de2b5df94d9a22b5f5'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.2.3.exe'
+  checksum      = 'd3a44cb6702532c7a1915fba8fed6528eb673c1e4b79096aa5bf1bc8240889a1'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
