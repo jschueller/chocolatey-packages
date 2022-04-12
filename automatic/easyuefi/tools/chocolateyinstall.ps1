8bbf066e14f975b85ba777f6329b5033db4c4707
chtof <chtof62@gmail.com>
2022-04-12 22:57:53 +0200

    Update chocolateyinstall.ps1

--- automatic/easyuefi/tools/chocolateyinstall.ps1
+++ automatic/easyuefi/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://www.easyuefi.com/downloads/EasyUEFI_Trial.exe'
-  checksum      = ''
+  checksum      = '2c36f2a45ae5719fab1cea2ccf28696d06a8e1c1fe23d48afa3873e59dcb19bc'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
