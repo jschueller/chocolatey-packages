c1338a46540b2cda84192458008ab0153fcb8de1
Chocolatey <chocolatey@realdimensions.net>
2022-04-24 16:25:38 +0000

    AU: 1 updated - klayout [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f4a57c8e578b70164d74ed55bf6dd0e045dc8cc4

--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.27.7-win32-install.exe'
-  checksum      = '657e6db058b1e2548c67a0367f34ab517757e3c6a30b835743e6ad0c9157823f'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.27.9-win32-install.exe'
+  checksum      = 'fabb30ceba178e42f2482da4bbca3af0ef4d72a3cf71f9cf7bc91469e7bc5c0e'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.27.7-win64-install.exe'
-  checksum64    = 'be7d5eed482588be195c0b76fc839f40bd409141efa1c7cef99f18596b837320'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.27.8-win64-install.exe'
+  checksum64    = '0eade674ed148d631916ad73b739eb139d12824df2f74f24a3fce3a4dae43de3'
 
   checksumType64= 'sha256'
 
