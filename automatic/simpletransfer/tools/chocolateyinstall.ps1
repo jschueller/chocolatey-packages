5658f500a558f0142be340cd022010fe4c1b5891
Chocolatey <chocolatey@realdimensions.net>
2022-04-27 08:25:49 +0000

    AU: 1 updated - simpletransfer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/0bbc64354a36f48f1932c555b836b9a7253de0c2

--- automatic/simpletransfer/tools/chocolateyinstall.ps1
+++ automatic/simpletransfer/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.4.0.exe'
-  checksum      = '60f1b7fe647334e5f02e8105c3d9604302813b98a738e9b80bbe92fedfd3477f'
+  url           = 'https://rambax.com/simpletransfer/desktop/SimpleTransfer%20Desktop%20Setup%203.4.1.exe'
+  checksum      = 'afa5971b0f83dcc4fd605564c33a7fa0b326ed523662091656faab11bc8df29e'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
