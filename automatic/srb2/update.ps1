d1f43a47a0805208524a071a20a9ea01ef9e50e9
chtof <chtof62@gmail.com>
2022-04-30 15:28:20 +0200

    Create update.ps1

---
+++ automatic/srb2/update.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module au
+Import-Module "..\..\helpers\GitHub_Helper.psm1"
+
+function global:au_GetLatest {
+    return github_GetInfo -ArgumentList @{
+        repository = 'STJr/SRB2'
+        regex      = 'SRB2_release_(?<Version>[\d\.]+)/SRB2-v\d+-Installer.exe'
+        }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
