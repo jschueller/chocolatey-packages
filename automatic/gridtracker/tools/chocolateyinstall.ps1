a6004d666abd7933de55773acb1ab2fd9269ac81
Chocolatey <chocolatey@realdimensions.net>
2022-05-03 00:25:30 +0000

    AU: 2 updated - eddie.install gridtracker [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/1d0961780d5d4778f9f86d2a967a5f728bdeb461

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://storage.googleapis.com/gt_download/v1.22.0502/GridTracker-Installer.1.22.0502.exe'
-  checksum     = '940aeb5dbe201d9b2630e774f2867de93c1379b5561bc372a6de00216b59ef24'
+  url          = 'https://storage.googleapis.com/gt_download/v1.21.0503-2/GridTracker-Installer.1.22.0503.exe'
+  checksum     = 'e641fd83bf281e11364eb138e906b6e85093c7504cf06bbb08605503d1d0e152'
   checksumType = 'sha256'
 
   silentArgs   = '/S'
