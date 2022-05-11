1b9c7758953077bc8afc381d485a88580c976363
Chocolatey <chocolatey@realdimensions.net>
2022-05-11 00:25:56 +0000

    AU: 5 updated - ocenaudio ocenaudio.install ocenaudio.portable tesseract virtualmidisynth [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/a3a133e429ab7cbcf0fa8ca28e4cc32ebcbb62ee

--- automatic/virtualmidisynth/tools/chocolateyinstall.ps1
+++ automatic/virtualmidisynth/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.12.6.exe"
+  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.12.7.exe"
   silentArgs  = '/S'
 }
 
