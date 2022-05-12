76160920cef83cb613cf24ca39fba14e1ab3818e
Chocolatey <chocolatey@realdimensions.net>
2022-05-12 00:25:40 +0000

    AU: 2 updated - cuda goodsync [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/eee35ae0abf481cc91c3008524b258c77200dbcb

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = '8016480905f2fb67bf69b279a16a12b38230fb10aa05c1dbed5c0afc862448c0'
+  checksum       = '2423c73f085db9d48ddc376395b5e3b3674e72fb46660d0f2d56e5ab025e3f91'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
