3795739a03b012f985a96e454a6e76fb7a61cfa7
chtof <chtof62@gmail.com>
2022-05-15 22:36:55 +0200

    Update chocolateyuninstall.ps1

--- automatic/launchyqt/tools/chocolateyuninstall.ps1
+++ automatic/launchyqt/tools/chocolateyuninstall.ps1
@@ -1,8 +1,4 @@
-﻿# Stop Launchy
-$process = Get-Process Launchy | Out-Null
-if ($process) { $process | Stop-Process }
-
-# Remove start menu shortcut
+﻿# Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Launchy.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
