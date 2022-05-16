d26c1e7e2b2518979ab3dc460ab14f1f7639997c
Chocolatey <chocolatey@realdimensions.net>
2022-05-16 16:26:08 +0000

    AU: 3 updated - activitywatch geogebra6 geogebra-geometry [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/0cd0b9bfde9e0b174847156826620d56166eab5e

--- automatic/activitywatch/tools/chocolateyinstall.ps1
+++ automatic/activitywatch/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/ActivityWatch/activitywatch/releases/download/v0.12.0b1/activitywatch-v0.12.0b1-windows-x86_64.zip'
-  checksum      = '53af843d3d332c05f25a7f2ebf25d0db98a064be5f028aeffc45b4973546819c'
+  url           = 'https://github.com/ActivityWatch/activitywatch/releases/download/v0.12.0b2/activitywatch-v0.12.0b2-windows-x86_64.zip'
+  checksum      = 'd5c1d0bebdfcecc8e801ba90a75c0928fe8ef8dffd68e1765be4d0d551cc7558'
   checksumType  = 'sha256'
 }
 
