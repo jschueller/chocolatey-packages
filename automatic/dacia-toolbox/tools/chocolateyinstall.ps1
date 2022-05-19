12febc6761f697ff3c27c044f5930fc67c84b23e
Chocolatey <chocolatey@realdimensions.net>
2022-05-19 08:25:28 +0000

    AU: 1 updated - dacia-toolbox [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/afe3188ec59df2d03e6c4afe8b2706da6beee53e

--- automatic/dacia-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-toolbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2022_01_05_10_59/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
-  checksum     = 'a3f62456394bc9089398838356f1f1d7dbd018d1a6ec6461aa545e1afd874ea0'
+  url          = 'https://download.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2022_05_13_12_03/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
+  checksum     = 'd048aa614726a5ba486f2d7828296ad2b04540cecbb96696f6e1495eb30c1dc8'
   checksumType = 'sha256'
 
   silentArgs   = "/S"
