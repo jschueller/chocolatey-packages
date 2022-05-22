bc64aefd73984d763d583271e1228bcde65b24e5
Chocolatey <chocolatey@realdimensions.net>
2022-05-22 08:25:45 +0000

    AU: 2 updated - crypto-notepad jtalert [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/473ba4c3b94fb7248b8b09bd3f10b98c478041a2

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dnl.hamapps.com/JTAlert/f3433bc7149504c748fd22307174cf3c'
-  checksum     = '78e286b54853205c6df31ef2448e5273054929dac277e0135af23dddd59d5452'
+  url          = 'https://dnl.hamapps.com/JTAlert/0aeffb87d29c6309d2602ea6bd7b3887'
+  checksum     = '7c70f0cb1671eb7e9adf87b181c4094a777d4232464624e615594f73798b3409'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
