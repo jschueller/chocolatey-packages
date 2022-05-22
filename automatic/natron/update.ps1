7d8ce8b2582f0b35a62d6e81132663bbef8afd0d
chtof <chtof62@gmail.com>
2022-05-22 19:26:52 +0200

    Update update.ps1

--- automatic/natron/update.ps1
+++ automatic/natron/update.ps1
@@ -3,9 +3,9 @@
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update -ChecksumFor none
+update -NoCheckUrl -ChecksumFor none
