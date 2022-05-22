6e3f63ac5f108e918248a908f2ad24ab67d42629
chtof <chtof62@gmail.com>
2022-05-22 19:13:38 +0200

    Update chocolateyinstall.ps1

--- automatic/natron.install/tools/chocolateyinstall.ps1
+++ automatic/natron.install/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   destination    = "$toolsDir"
 
   url64          = 'https://github.com//NatronGitHub/Natron/releases/download/v2.4.3/Natron-2.4.3-Windows-x86_64.zip'
-  checksum64     = ''
+  checksum64     = '5bd66ac2da636f89f1d9e7622f6e25010d67853aa8244ea7e3e1d9bbbd790c40'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
