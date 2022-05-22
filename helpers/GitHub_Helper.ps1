8e598852da7ebd9f5d6c27c58981bbe53b5aff0b
chtof <chtof62@gmail.com>
2022-05-22 11:08:05 +0200

    Update GitHub_Helper.ps1

--- helpers/GitHub_Helper.ps1
+++ helpers/GitHub_Helper.ps1
@@ -2,22 +2,45 @@ function github_GetInfo {
     Param([array]$ArgumentList)    
 
     $debug = 0;
-    $github_url = 'https://github.com'
-    $github_repository_root     = "$github_url" + $ArgumentList.repository
+    $github_url = 'https://github.com/'
+    $github_repository_root     = "$github_url" + $ArgumentList.repository    
     $github_repository_releases = "$github_repository_root" + '/releases/latest'
+    $regex32 = $ArgumentList.regex32;
+    $regex64 = $ArgumentList.regex64;
+    $output = @()
 
-    $url32_path = ($download_page.links | ? href -match $ArgumentList.regex | select -Last 1).href
+    $download_page = Invoke-WebRequest -Uri $github_repository_releases -UseBasicParsing
+
+    If ($regex64) {
+        $uri64_path = ($download_page.links | ? href -match $regex64 | select -Last 1).href
+        $output += @{ URL64 = $github_url + $uri64_path }
+        If ($matches.Version) {
+            $output += @{ Version = $matches.Version }
+         
+        }
+    }
+    If ($regex32) {
+        $uri32_path = ($download_page.links | ? href -match $regex32 | select -Last 1).href
+        $output     += @{ URL32 = $github_url + $uri32_path }
+        If ($matches.Version) {
+            $output += @{ Version = $matches.Version }
+         
+        }
+    }
+
+    If (-Not($output.Version)) { Write-Warning "Version is missing."}
+    If ((-Not($output.URL32))-And(-Not($output.URL64))) { Write-Warning "URL32 and/or URL64 are missing."}
 
     If ($debug) {
         Write-Host "github_repository_root:" $github_repository_root
         Write-Host "github_repository_releases:" $github_repository_releases
-        Write-Host "Regex:" $ArgumentList.regex
+        Write-Host "Regex32:" $ArgumentList.regex32
+        Write-Host "Regex64:" $ArgumentList.regex64
         Write-Host "Version:" $matches.Version
-        Write-Host "url32_path: " $url32_path
+        Write-Host "uri32_path: " $uri32_path
+        Write-Host "uri64_path: " $uri64_path
+        Write-Host "output: " ($output | Format-Table | Out-String)
     }
 
-    return @{
-        Version = $matches.Version;
-        URL32 = $github_url + $url32_path;
-    }
+    return $output
 }
