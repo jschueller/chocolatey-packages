b8fc0dc087093159932d4ce19b63fced41927d4b
chtof <chtof62@gmail.com>
2022-05-26 14:48:00 +0200

    Update chocolateyinstall.ps1

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿c$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
 
 $packageArgs = @{
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = 'f55943f5ddb628bc8a0f648d442f9fb0c4655bb56d629a349d9b22f609704b95'
+  checksum      = 'b38c83f24426f90d5ebec83e8a150a88f4b2f253a653cb237b96c8a4add76c9d'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '77fc2c4bb2e28e872b660918a0cb870428d9569e8b612a0a6fc22fa7557ddf2e'
+  checksum64    = 'f23cff42cb1a4ddcf6b6650c5296a633cb85b07d7a2d87db420087202f8555f3'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
@@ -21,3 +21,10 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyInstallPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$target = "$env:LOCALAPPDATA\keeperpasswordmanager"
+$shortcutFilePath = Join-Path $programs "Keeper.lnk"
+$targetPath = Join-Path $target "keeperpasswordmanager.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
