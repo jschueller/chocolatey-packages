99aef97e0d55fe4171d307f4af9b1d8738501ecb
Chocolatey <chocolatey@realdimensions.net>
2022-05-31 08:25:57 +0000

    AU: 4 updated - chirp chirp.install chirp.portable internet-download-manager [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fe847b981f1092ffae12939a6b13bd27548c401b

--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"
-  file          = "$toolsDir\chirp-daily-20220515-win32.zip"
+  file          = "$toolsDir\chirp-daily-20220531-win32.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman641build1.exe'
-  checksum      = 'd73f71e7ad61dfb8098fd9992c9491592e421a089ad9c05a273b894632bd2ec3'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman641build2.exe'
+  checksum      = '025eac1fad5cdb0442bff25b2bcc80e28f28ac97910dd88f5b750fc06dc4b29e'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
