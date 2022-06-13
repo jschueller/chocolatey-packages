6e5357016ba784c4c9a2c70caa2152d828f43233
Chocolatey <chocolatey@realdimensions.net>
2022-06-13 16:26:15 +0000

    AU: 4 updated - chirp chirp.install chirp.portable geogebra6 [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/391575687138cec36148947062dc8451ae639f4d

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20220604-installer.exe"
+  file          = "$toolsDir\chirp-daily-20220613-installer.exe"
   silentArgs    = '/S'
 }
 
